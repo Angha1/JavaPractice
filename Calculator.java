e39cf13020d33c019e4a866408dbc72557a64166
Angha1 <131744268+Angha1@users.noreply.github.com>
2023-05-15 16:56:22 +0530

    This is a Calculator Program
    It includes all arithmetic operations.

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+ public static void main(String[] args){
+int x=90;
+int y=20;
+  System.out.println("Addition of two numbers:" +(x+y));
+  System.out.println("Substraction of two numbers:" +(x-y));
+  System.out.println("Multiplication of two numbers:" +(x*y));
+  System.out.println("Division of two numbers:" +(y/x));
+  System.out.println("Remainder of two numbers:" +(x%y));
+ }
+}
