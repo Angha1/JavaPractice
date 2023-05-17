76584b47525074525e6b3da1ef210f5f5bac4dd7
Angha1 <131744268+Angha1@users.noreply.github.com>
2023-05-17 11:47:45 +0530

    This code contains Size of fundamental data types.

---
+++ Datatypes.java
@@ -0,0 +1,16 @@
+// Write a program in java to find Size of fundamental data types.   
+
+
+
+public class Datatypes{
+  public static void main(String[] args){
+System.out.println("size of fundamental data types in java (in bytes):");
+System.out.println("The size of(char) is:"+Character.SIZE/8);
+System.out.println("The size of(short) is:"+Short.SIZE/8);
+System.out.println("The size of(int) is:"+Integer.SIZE/8);
+System.out.println("The size of(long) is:"+Long.SIZE/8);
+System.out.println("The size of(float) is:"+Float.SIZE/8);
+System.out.println("The size of(double) is:"+Double.SIZE/8);
+System.out.println("The size of(bool) is : 1 bytes");
+   }
+}
